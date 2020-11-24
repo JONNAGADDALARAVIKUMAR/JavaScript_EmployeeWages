f8afecfc9d9a13ded6a778682df381477c3105cc
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-24 20:42:31 +0530

    Refactored: Storing Daily Wage along with Total Wage

--- employeeWages.js
+++ employeeWages.js
@@ -4,35 +4,49 @@ let wagePerHour = 20;
 let fullTimeHours = 8;
 let partTimeHours = 4;
 let totalHours = 0;
+let dailyWage;
+const dailyAndTotalWage = [];
 
 let randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min;//Generates Random Number between two numbers
 
-const getTotalHours = (time) => { //method to get total working Hours
+const getTotalHours = (workingHoursPerDay, workingHoursPerMonth) => { //method to get total working Hours
     for(let i = 1; i <= 20; i++) {
-        if(randomValue(1, 6) < 4) {
-           totalHours += time;
-        }
+       
+        if(randomValue(1, 6) < 4 && totalHours < workingHoursPerMonth) {
+           totalHours += workingHoursPerDay;
+           dailyWage = workingHoursPerDay * wagePerHour;
+       
+           if(totalHours > workingHoursPerMonth) {
+               totalHours -= workingHoursPerDay / 2;
+               dailyWage = workingHoursPerDay * (wagePerHour / 2);
+           }
+           dailyAndTotalWage.push(dailyWage, totalHours * 20);
+        } else {
+            dailyAndTotalWage.push(0, totalHours * 20);
+        } 
     }
     return totalHours;
 }
 
-let totalWage = (time, workingHoursPerMonth) => { // Calculates total Wage
-    totalHours = getTotalHours(time);
-    if(totalHours > workingHoursPerMonth) {
-        totalHours = workingHoursPerMonth;
-    }
-    console.log('No of working days : ', totalHours / time, 
+let totalWage = (workingHoursPerDay, workingHoursPerMonth) => { // Calculates total Wage and stores in array
+    totalHours = getTotalHours(workingHoursPerDay, workingHoursPerMonth);
+    console.log('No of working days : ', totalHours / workingHoursPerDay, 
                 '\nHours Worked : ', totalHours);
+    
+    console.log('Daily Wage\tTotal Wage');
+    for(let i = 0; i < dailyAndTotalWage.length; i = i+2) {
+        console.log(dailyAndTotalWage[i], '\t\t', dailyAndTotalWage[i+1]);
+    }
     return wagePerHour * totalHours;
 }
 
 switch(randomValue(1, 3)) {
     case 1:
         console.log('Full Time Employee');
-        console.log('Total Wage : ', totalWage(fullTimeHours, 100));
+        console.log('\nTotal Wage : ', totalWage(fullTimeHours, 100));
         break;
     case 2:
         console.log('Part Time Employee');
-        console.log('Total Wage : ', totalWage(partTimeHours, 50));
+        console.log('\nTotal Wage : ', totalWage(partTimeHours, 50));
         break;
 }
