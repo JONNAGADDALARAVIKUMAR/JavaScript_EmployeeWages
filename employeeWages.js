7752d0101eee0908d63d1e5ae38da9b021db17af
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-24 17:02:05 +0530

    Added: Calculating Full Time and Part Time wages

--- employeeWages.js
+++ employeeWages.js
@@ -1,13 +1,15 @@
 console.log('\n\tWelcome to Employee Wage Computation Program\n');
-let attendence = Math.floor(Math.random() * 5) + 1;
-let wagePerHour = 20;
-let time = 8;
-let totalWage;
 
-if(attendence > 2) {
-    console.log('Employee Present');
-    totalWage = wagePerHour * time;
-    console.log('Total Wage : ', totalWage);
+let wagePerHour = 20;
+let fullTime = 8;
+let partTime = 4;
+
+let randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+let totalWage = (time) => wagePerHour * time;
+if(randomValue(1, 5) > 2) {
+    console.log('Employee is Present');
+    console.log('Full Time Wage : ', totalWage(fullTime));
+    console.log('Part Time Wage : ', totalWage(partTime));
 } else {
     console.log('Employee Absent');
 }
