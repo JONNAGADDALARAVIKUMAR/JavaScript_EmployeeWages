5692f3c2573b85398b49c94cc44aba25e54538a5
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-24 16:11:12 +0530

    Added: Calculating Daily Wages

--- employeeWages.js
+++ employeeWages.js
@@ -1,8 +1,13 @@
 console.log('\n\tWelcome to Employee Wage Computation Program\n');
 let attendence = Math.floor(Math.random() * 5) + 1;
+let wagePerHour = 20;
+let time = 8;
+let totalWage;
 
 if(attendence > 2) {
     console.log('Employee Present');
+    totalWage = wagePerHour * time;
+    console.log('Total Wage : ', totalWage);
 } else {
     console.log('Employee Absent');
 }
