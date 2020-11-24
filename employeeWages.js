f5002ea1778eff37b9cc9fa80a8a5f561b44a3c7
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-24 17:28:35 +0530

    Added: Calculating Full Time or Part time Wages by using switch statement

--- employeeWages.js
+++ employeeWages.js
@@ -6,10 +6,19 @@ let partTime = 4;
 
 let randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 let totalWage = (time) => wagePerHour * time;
-if(randomValue(1, 5) > 2) {
+
+if(randomValue(1, 6) > 2) {
     console.log('Employee is Present');
-    console.log('Full Time Wage : ', totalWage(fullTime));
-    console.log('Part Time Wage : ', totalWage(partTime));
+    switch(randomValue(1, 3)) {
+        case 1:
+            console.log('Full Time Wage : ', totalWage(fullTime));
+            break;
+        case 2:
+            console.log('Part Time Wage : ', totalWage(partTime));
+            break;
+        default:
+            console.log('Fail');
+    }
 } else {
     console.log('Employee Absent');
 }
