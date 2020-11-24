9ea1f94046198c1dd74d69423a5e8ac59a85ae3b
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-24 15:57:46 +0530

    Added: Employee Present or Not

--- employeeWages.js
+++ employeeWages.js
@@ -1,1 +1,8 @@
 console.log('\n\tWelcome to Employee Wage Computation Program\n');
+let attendence = Math.floor(Math.random() * 5) + 1;
+
+if(attendence > 2) {
+    console.log('Employee Present');
+} else {
+    console.log('Employee Absent');
+}
