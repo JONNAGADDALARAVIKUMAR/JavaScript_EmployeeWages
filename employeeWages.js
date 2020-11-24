256fc3347a8de1dc63af4b2272368d0d486e0789
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-24 18:48:29 +0530

    Added: Calculating Wages for a Month and specified hours

--- employeeWages.js
+++ employeeWages.js
@@ -1,29 +1,36 @@
 console.log('\n\tWelcome to Employee Wage Computation Program\n');
 
 let wagePerHour = 20;
-let fullTime = 8;
-let partTime = 4;
+let fullTimeHours = 8;
+let partTimeHours = 4;
 let totalHours = 0;
 
 let randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-let totalWage = (time) => {
+let totalWage = (time, workingHoursPerMonth) => {
     for(let i = 1; i <= 20; i++) {
         if(randomValue(1, 6) > 2) {
            totalHours += time;
+           
+           if(totalHours > workingHoursPerMonth) {
+                totalHours -= time / 2;
+            }
+           if(totalHours == workingHoursPerMonth) {
+                break;
+           }
         }
     }
-    console.log('No of working days : ', totalHours / time);
+    console.log('No of working days : ', totalHours / time, '\nHours Worked : ', totalHours);
     return wagePerHour * totalHours;
 }
 
 switch(randomValue(1, 3)) {
     case 1:
         console.log('Full Time Employee');
-        console.log('Total Wage : ', totalWage(fullTime));
+        console.log('Total Wage : ', totalWage(fullTimeHours, 100));
         break;
     case 2:
         console.log('Part Time Employee');
-        console.log('Total Wage : ', totalWage(partTime));
+        console.log('Total Wage : ', totalWage(partTimeHours, 50));
         break;
 }
